96f67b59c1c8de11da1d15cb95897d48f7ab48eb
Steve Lipton <stevelipton@brk-16892.local>
2018-05-17 05:59:39 -0700

    initial commit end state

---
+++ UIColorExtension/UIColorExt.swift
@@ -0,0 +1,40 @@
+//
+//  UIColorExt.swift
+//  UIColorExtension
+//
+//  Created by Steve Lipton on 5/17/18.
+//  Copyright © 2018 Steve Lipton. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    class var tomato:UIColor{
+        return self.init(red:0.67,green:0.0,blue:0.0,alpha: 1.0)
+    }
+    class var cheese:UIColor{
+        return self.hexColor(0xfdff54, alpha: 1.0)
+    }
+    class var crust:UIColor{
+        return self.hexColor(0xe39448, alpha: 1.0)
+    }
+    class func hexColor(_ hexColorNumber:UInt32, alpha: CGFloat) -> UIColor {
+        let red = (hexColorNumber & 0xff0000) >> 16
+        let green = (hexColorNumber & 0x00ff00) >> 8
+        let blue =  (hexColorNumber & 0x0000ff)
+        return self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: alpha)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- UIColorExtension/ViewController.swift
+++ UIColorExtension/ViewController.swift
@@ -1,19 +1,22 @@
 //
 //  ViewController.swift
-//  UIColorExtension
+//  ColorExtension
 //
-//  Created by Steve Lipton on 5/17/18.
-//  Copyright © 2018 Steve Lipton. All rights reserved.
+//  Created by Steven Lipton on 5/15/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+   let burntCrust = UIColor.hexColor(0x251a08, alpha: 1.0)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .cheese
     }
+    
+     
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
